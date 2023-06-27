640733cf8e9a1a3c88948c9d2435b7f673378ae9
Monica Carbajal <monica.carbajal2@gmail.com>
2023-06-26 20:50:46 -0600

    eqArray.js

---
+++ eqArrays.js
@@ -0,0 +1,35 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ True: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 False: ${actual} !== ${expected}`);
+  }
+};
+
+function eqArrays(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++)
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+
+  return true;
+}
+
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+
+
+
+
+//   return true
+// }
+  
